90518aa149c9b87f76d165b6a9c7ae599f10905e
Emin Topalovic <electronic_mint@emint.awesome>
2012-03-03 23:15:58 -0800

    Added bullet into the project. Started setting up the camera control. Need to do mouse next.

--- Makefile
+++ Makefile
@@ -1,10 +1,20 @@
 #Note that this expects your SFML libraries and headers to be
 #installed as necessary
+
+#Also asssumes that bullet physiscs is installed in common path.
+
+#-Wno-switch to prevent the compiler from complaining about not 
+#including all possible window events in switch statement.
+CXXFLAGS = -g -Wall -Wno-switch -I/usr/local/include/bullet 
 LIBS = -lsfml-network \
 	-lsfml-window \
 	-lsfml-graphics \
 	-lsfml-system \
-	-lGLU
+	-lGLU \
+	-lBulletCollision \
+	-lBulletDynamics \
+	-lBulletCollision \
+	-lLinearMath 
 	
 OBJS = main.o
 
--- main.cpp
+++ main.cpp
@@ -1,56 +1,105 @@
+#include <iostream>
+
 #include <GL/glu.h>
 #include <SFML/Window.hpp>
 #include <SFML/Graphics.hpp>
 
+#include <bullet/btBulletDynamicsCommon.h>
+
+using namespace std;
+/**
+ * Camera and movement variables
+ */
+float eye_x = 0.0f;
+float eye_y = 0.0f;
+float eye_z = 1.0f;
+
+//Set-up the window settings and get a handle to a window
 sf::WindowSettings settings(24, 8, 2);
 sf::Window window(sf::VideoMode(800, 600), "CS248 Rules!", sf::Style::Close,
     settings);
 
 void glInit() {
-  //Viewing
   glViewport(0, 0, window.GetWidth(), window.GetHeight());
 
-  // Set color and depth clear value
   glClearDepth(1.f);
   glClearColor(0.6f, 0.3f, .2f, 0.f);
 
-  // Enable Z-buffer read and write
   glEnable(GL_DEPTH_TEST);
   glDepthMask(GL_TRUE);
 
-  // Setup a perspective projection
+  glEnable(GL_BLEND);
+  glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+}
+
+void keyPressed(sf::Key::Code key) {
+  if (key == sf::Key::A) {
+    eye_x -= .1f;
+  } else if (key == sf::Key::D) {
+    eye_x += .1f;
+  } else if (key == sf::Key::W) {
+    eye_z -= .6f;
+  } else if (key == sf::Key::S) {
+    eye_z += .6f;
+  }
+}
+
+void handleInput() {
+  sf::Event evt;
+  while (window.GetEvent(evt)) {
+    switch (evt.Type) {
+      case sf::Event::Closed:
+        window.Close();
+        break;
+      case sf::Event::KeyPressed:
+        keyPressed(evt.Key.Code);
+        cout<<eye_x<<" "<<eye_y<<" "<<eye_z<<endl;
+        break;
+    }
+  }
+}
+
+void setupViewAndProjection() {
   glMatrixMode(GL_PROJECTION);
   glLoadIdentity();
   gluPerspective(90.f, 1.f, 1.f, 500.f);
 
-  //  Set the camera
   glMatrixMode(GL_MODELVIEW);
-  gluLookAt(0.0f, 0.0, 2, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0);
+  glLoadIdentity();
+  gluLookAt(eye_x, eye_y, eye_z, eye_x, eye_y, eye_z - 2, 0.0, 1.0, 0.0);
+}
+
+void renderPlayerAvatar() {
+  glColor4f (.1, .6, .4, 1);
+
+  glBegin(GL_QUADS);
+  glVertex3f(eye_x - .1, eye_y - .1, eye_z - 2);
+  glVertex3f(eye_x - .1, eye_y + .1, eye_z - 2);
+  glVertex3f(eye_x + .1, eye_y + .1, eye_z - 2);
+  glVertex3f(eye_x + .1, eye_y - .1, eye_z - 2);
+  glEnd();
+}
+void renderScene() {
+  setupViewAndProjection();
+
+
+  glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+  renderPlayerAvatar();
+
+  glColor4f(.23, .25, .9, 1);
+  glBegin(GL_TRIANGLES);
+  glVertex3f(-.5, 0, -1);
+  glVertex3f(.5, 0, -1);
+  glVertex3f(0, .5, -1);
+  glEnd();
 }
 
 int main() {
   glInit();
   while (window.IsOpened()) {
-    // Event loop, for processing user input, etc.  For more info, see:
-    // http://www.sfml-dev.org/tutorials/1.6/window-events.php
-    sf::Event evt;
-    while (window.GetEvent(evt)) {
-      switch (evt.Type) {
-        case sf::Event::Closed:
-          // Close the window.  This will cause the game loop to exit,
-          // because the IsOpened() function will no longer return true.
-          window.Close();
-          break;
-      }
-    }
-
-    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-    glColor3f(.23, .25, .9);
-    glBegin(GL_TRIANGLES);
-    glVertex3f(-.1, 0, 0);
-    glVertex3f(.1, 0, 0);
-    glVertex3f(0, .1, 0);
-    glEnd();
+    handleInput();
+    renderScene();
     window.Display();
   }
 }
